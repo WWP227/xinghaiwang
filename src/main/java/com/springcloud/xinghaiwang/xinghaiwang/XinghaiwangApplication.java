8eb24091bc293db552a2085fe2ac9a2db5c66624
WWP227 <1073942427@qq.com>
2020-01-13 09:31:26 +0800

    架子

---
+++ src/main/java/com/springcloud/xinghaiwang/xinghaiwang/XinghaiwangApplication.java
@@ -0,0 +1,13 @@
+package com.springcloud.xinghaiwang.xinghaiwang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XinghaiwangApplication{
+
+    public static void main(String[] args){
+        SpringApplication.run(XinghaiwangApplication.class,args);
+    }
+
+}
